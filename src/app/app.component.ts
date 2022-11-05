12faf6237dfa8d53ffed7e92bee9a5553eb103a8
Darko <darko.l@enginee.rs>
2022-11-05 14:28:56 +0100

    added flexLayout config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {MediaObserver} from "@angular/flex-layout";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private mediaSubscription: Subscription;
+  private activeMediaQuery = '';
   title = 'conversion-calculator';
+  deviceXs: boolean;
+
+  constructor(public mediaObserver: MediaObserver) { }
+
+  ngOnInit() {
+    this.mediaSubscription = this.mediaObserver
+      .asObservable()
+      .subscribe((change) => {
+        change.forEach((item) => {
+          this.activeMediaQuery = item
+            ? `'${item.mqAlias}' = (${item.mediaQuery})`
+            : '';
+          if (item.mqAlias === 'xs') {
+            this.deviceXs = true;
+          }
+          console.log('activeMediaQuery', this.activeMediaQuery);
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.mediaSubscription.unsubscribe();
+  }
 }
